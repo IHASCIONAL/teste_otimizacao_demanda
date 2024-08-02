b3236508026a116b996553bea39deea480af2170
Ismael <ismaelhenriqueeu@gmail.com>
2024-08-02 18:33:02 -0300

    build: gerar baseline no frontend

---
+++ src/frontend.py
@@ -0,0 +1,59 @@
+import streamlit as st
+
+class OrdersReader:
+
+    def __init__(self):
+        self.set_page_config()
+
+    def set_page_config(self):
+        st.set_page_config(
+            page_title="Gerador de Baseline"
+        )
+
+    def display_header(self):
+        st.title("Gerador de Baseline")
+
+    def data_inicial(self):
+        st.write("Selecione o intervalo de datas para filtrar a tabela de pedidos que será utilizada para gerar o baseline!")
+        start_date = st.date_input("Data inicial de corte para o baseline", key="start_date")
+        return start_date
+    
+    def data_final(self):
+          end_date = st.date_input("Data final de corte para o baseline", key="end_date")
+          return end_date
+    
+    def data_entrega_demanda(self):
+          delivery_date = st.date_input("Data em que a demanda será entregue", key="delivery_date")
+          return delivery_date
+
+    def mes_seguinte(self):
+        incluir_mes_seguinte = st.checkbox("Incluir mês seguinte na previsão?", key="incluir_mes_seguinte")
+        return incluir_mes_seguinte
+    
+    def upload_file(self):
+        return st.file_uploader("Carregue o arquivo excel com o histórico de Pedidos aqui!", type=["xlsx"])
+    
+    def display_processing_message(self):
+        self.processing_placeholder = st.empty()
+        with self.processing_placeholder:
+            with st.spinner("Processando o arquivo..."):
+                pass
+    
+    def update_processing_message(self, message):
+        if hasattr(self, 'processing_placeholder'):
+            self.processing_placeholder.text(message)
+        else:
+            st.toast(message)
+
+    def display_wrong_message(self):
+        st.error("Houve um problema durante o processamento. Verifique os erros.")
+
+
+    def display_results(self, result, errors, df):
+        if errors:
+            for error in errors:
+                st.error(f"Erro na validação: {error}")
+        else:
+            st.success("O schema do arquivo Excel está correto!")
+            st.success("Aqui está o seu baseline!")
+            st.dataframe(df)
